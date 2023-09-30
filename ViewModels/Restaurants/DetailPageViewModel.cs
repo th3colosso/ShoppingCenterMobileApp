f897d4c407344f98e2b82b9fa5f40abfdb9ef0a7
Matheus Correa <mattheuuz@outlook.com>
2023-09-30 16:51:08 -0300

    added detail page to restaurants

---
+++ ViewModels/Restaurants/DetailPageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using ShoppingCenterApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCenterApp.ViewModels.Restaurants {
+
+    [QueryProperty(nameof(Establishment), "establishment")]
+    public partial class DetailPageViewModel : ObservableObject {
+        [ObservableProperty]
+        private Establishment _establishment;
+
+        [RelayCommand]
+        private void GoBackClicked() => Shell.Current.GoToAsync("..");
+    }
+}
