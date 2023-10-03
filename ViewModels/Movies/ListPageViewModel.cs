b3496b5f0ff849b352c178b1140544a5efbf3259
Matheus Correa <mattheuuz@outlook.com>
2023-10-03 13:56:17 -0300

    init config listpage movies

---
+++ ViewModels/Movies/ListPageViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using ShoppingCenterApp.Models;
+using ShoppingCenterApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCenterApp.ViewModels.Movies {
+    public partial class ListPageViewModel : ObservableObject {
+
+        [ObservableProperty]
+        private List<Movie> movies;
+
+        public ListPageViewModel() {
+            var service = App.Current.Handler.MauiContext.Services.GetService<MovieService>();
+            Movies = service.GetAll();
+        }
+
+        [RelayCommand]
+        private void OpenMovieDetailPage(Movie movie) {
+            var param = new Dictionary<string, object> {
+                {"movie", movie }
+            };
+            Shell.Current.GoToAsync("detail", param);
+        }
+    }
+}
